d541f7fbaf4edf5348412f5eea894ad83632c622
Sachin <Sachin@TYSS-SACHIN>
2024-06-18 11:38:38 +0530

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GitPractise
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("hai");
+        }
+    }
+}
