ca34483e9f7650a19afa0f7c9a7ce61e16265eb9
Sachin <Sachin@TYSS-SACHIN>
2024-06-18 11:41:24 +0530

    1 commit

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -10,7 +10,7 @@ namespace GitPractise
         [Test]
         public void Test1()
         {
-            Console.WriteLine("hai");
+            Console.WriteLine("hai sachin");
         }
     }
 }
